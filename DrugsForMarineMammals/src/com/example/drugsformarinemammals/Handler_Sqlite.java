7cd84799ac213b8ab39b0ce1a7baca484cf93821
Edu <evillarroelcuadrado@gmail.com>
2014-11-20 11:37:54 +0100

    Update Handler_Sqlite.java by merge error

--- DrugsForMarineMammals/src/com/example/drugsformarinemammals/Handler_Sqlite.java
+++ DrugsForMarineMammals/src/com/example/drugsformarinemammals/Handler_Sqlite.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
