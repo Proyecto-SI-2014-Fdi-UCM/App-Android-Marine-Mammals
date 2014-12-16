26b160ab6b6882d4057517fdbb951be4e7b07ce6
Mayra <majuma22@gmail.com>
2014-12-16 16:34:26 +0100

    Update Fragment About

--- DrugsForMarineMammals/src/com/example/drugsformarinemammals/Fragment_About.java
+++ DrugsForMarineMammals/src/com/example/drugsformarinemammals/Fragment_About.java
@@ -78,14 +78,6 @@ public class Fragment_About extends Fragment {
 		startActivity(intent);
 	}
 		
-		
-	private void copyFile(InputStream in, OutputStream out) throws IOException {
-		byte[] buffer = new byte[1024];
-		int read;
-		while ((read = in.read(buffer)) != -1) {
-			out.write(buffer, 0, read);
-		}
-	}
 
 	private void initializeListViewWithImage() {
 
